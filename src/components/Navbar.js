b0ae80e55092ab267047d5db8846526f58451774
sogliola <79167877+sogliola@users.noreply.github.com>
2021-12-16 20:28:24 +0800

    Update Navbar.js
    edited navbar brand

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import { Navbar, Nav } from "react-bootstrap";
 export default function NavbarComponent() {
   return (
     <Navbar>
-      <Navbar.Brand href="#home">Eited NavBar</Navbar.Brand>
+      <Navbar.Brand href="#home">Edited NavBar</Navbar.Brand>
       <Nav classname="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
         <Nav.Link href="#link">Link</Nav.Link>
