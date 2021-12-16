322f62e243db4c78ffb185d066b7f55c3ce04cf4
sogliola <79167877+sogliola@users.noreply.github.com>
2021-12-16 21:18:02 +0800

    Update App.js
    added about me component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Card from "./components/Card";
 import Jumbo from "./components/Jumbo";
 import Footer from "./components/Footer";
+import About from "./components/About";
 
 export default function App() {
   return (
@@ -25,6 +26,7 @@ export default function App() {
           </Col>
         </Row>
       </Container>
+      <About />
       <Footer></Footer>
     </>
   );
