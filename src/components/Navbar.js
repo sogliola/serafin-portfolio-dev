a7921e30293acd6a2cfab9a5d5e771cd3e4290d9
Rey Serafin <reybserafin@gmail.com>
2021-12-16 19:58:11 +0800

    added Navbar features

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,11 +3,12 @@ import { Navbar, Nav } from "react-bootstrap";
 
 export default function NavbarComponent() {
   return (
-    <Navbar>
-      <Navbar.Brand href="#home">React Bootstrap</Navbar.Brand>
+    <Navbar bg="dark" variant="dark">
+      <Navbar.Brand href="#home">React Bootstrap 6th Gen</Navbar.Brand>
       <Nav classname="mr-auto">
         <Nav.Link href="#home">Home</Nav.Link>
-        <Nav.Link href="#link">Link</Nav.Link>
+        <Nav.Link href="#about">About Us</Nav.Link>
+        <Nav.Link href="#careers">Careers</Nav.Link>
       </Nav>
     </Navbar>
   );
